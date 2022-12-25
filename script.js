00f5d03e2e237608c25761a4d5eaeaf7ccafc0c8
shannonrumsey <shannonrumsey@ucsb.edu>
2022-12-24 19:47:52 -0800

    added more information and changed format

---
+++ script.js
@@ -0,0 +1,12 @@
+// add class navbarDark on navbar scroll
+const header = document.querySelector('.navbar');
+
+window.onscroll = function() {
+    var top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarLight');
+    }
+    else {
+        header.classList.remove('navbarLight');
+    }
+}
